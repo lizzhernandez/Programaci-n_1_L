825ed99f8d816283d51f87f777f12df820aadf4e
lizzhernandez <lizbet.hernandez1596@alumnos.udg.mx>
2026-09-23 10:43:41 -0600

    Add files via upload

---
+++ gfgt.py
@@ -0,0 +1,25 @@
+#git config --global user.name "lizzhernandez" 
+#git config --global user.email "lizbet.hernandez1596@alumnos.udg.mx"
+DIAS_SEMANA=7
+GRAVEDAD=9.81
+CALIFICACION=85
+EDAD=19
+print(DIAS_SEMANA)
+print(GRAVEDAD)
+print(CALIFICACION)
+print(EDAD)
+CONSTANTES={
+    "IVA": 0.15 }
+valor=1000
+total=valor + (valor * CONSTANTES ["IVA" ] )
+edad = 15 
+altura = 1.75
+nombre ="Ana"
+activo = True 
+
+print(5)
+print("hola")
+print(5 + 3)
+print(type(15))
+print(type(1.75))
+
